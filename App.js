ed580bf016a8b0bcb9b2ae03c0a7c3343d66f5fe
Odeide Moreira Junior <odeidemoreira@gmail.com>
2021-08-13 21:50:28 -0300

    comentários das alterações

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+
+import { TextInput, Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground style={styles.image} source={require('./src/images/teste1.png')}>
+        <TextInput label="Usuário" style={styles.textInput} underlineColor="black" selectionColor="gray" 
+          left={   <TextInput.Icon name="account" />  }
+        />
+        <TextInput label="Senha" style={styles.textInput} underlineColor="black"  icon="lock"
+          left={ <TextInput.Icon name="lock" /> }
+        />
+        <Button  color='#4bab8f' style={styles.botao} icon="login" mode="contained" onPress={() => console.log('Pressed')}>
+          Acessar
+      </Button>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+
     flex: 1,
+    marginLeft: 0,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  textInput: {
+    top: 50,
+    color: "white",
+    padding: 2,
+    margin: 10,
+    
+    marginLeft: 70,
+    height: 60,
+    width: '70%',
+
+    backgroundColor: 'transparent',
+
+  },
+  botao: {
+    top: 80,
+    marginLeft: 75,
+    height: 40,
+    width: '70%',
+   
+  }
 });
